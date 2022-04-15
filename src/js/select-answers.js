113e2cdf8449a0e23e38927acd0cee7a43445b31
Shaludnev D <junerkukas@ya.ru>
2022-04-15 20:20:21 +0600

    finally!

--- src/js/select-answers.js
+++ src/js/select-answers.js
@@ -1,19 +1,52 @@
-//находим чекбоксы с "простыми" ответами и чекбоксы с ответом, подразумеващим выбор всего
+// находим чекбоксы с "простыми" ответами и чекбоксы с ответом, подразумеващим выбор всего
 
 const answers = document.querySelectorAll('.list-item__checkbox:not(.select-all)')
 const selectAllAnswer = document.querySelector('.select-all')
 
-let checkedAnswers = 0
+// отслеживаем количество отмеченных вопросов
 
-const answerHandle = e => {
-  if (e.target.checked)
-    checkedAnswers ++
-  else
-    checkedAnswers--
+let checkedAnswersCount = 0
 
-  console.log(checkedAnswers, answers)
+// функция смены статуса чекбокса
+
+function check(checkboxElement) {
+  checkboxElement.checked = !checkboxElement.checked
 }
 
+// хендл на одинарный ответ
+
+function answerHandle (e) {
+  if (e.target.checked)
+    checkedAnswersCount ++
+  else {
+    checkedAnswersCount--
+    if (selectAllAnswer.checked)
+      check(selectAllAnswer)
+  }
+
+  if (checkedAnswersCount === answers.length)
+    check(selectAllAnswer)
+}
+
+// хендл на ответ для множественного выбора
+
+function selectAllHandle () {
+  if (selectAllAnswer.checked)
+    answers.forEach(answer => {
+      answer.checked = true
+      checkedAnswersCount = answers.length
+    })
+  else
+    answers.forEach(answer => {
+      answer.checked = false
+      checkedAnswersCount = 0
+    })
+}
+
+// ставим слушатели на события смены чекбоксов
+
 answers.forEach(answer => {
-  addEventListener('change', answerHandle)
+  answer.addEventListener('change', answerHandle)
 })
+
+selectAllAnswer.addEventListener('change', selectAllHandle)
