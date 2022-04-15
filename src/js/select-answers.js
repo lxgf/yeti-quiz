b854f16723b38a70e8ad06571cdb7b75ec84a7bf
Shaludnev D <junerkukas@ya.ru>
2022-04-15 13:58:42 +0600

    temp

--- src/js/select-answers.js
+++ src/js/select-answers.js
@@ -1,2 +1,19 @@
-let answers = document.querySelectorAll('.list-item')
-answers[0].style.background = 'black'
+//находим чекбоксы с "простыми" ответами и чекбоксы с ответом, подразумеващим выбор всего
+
+const answers = document.querySelectorAll('.list-item__checkbox:not(.select-all)')
+const selectAllAnswer = document.querySelector('.select-all')
+
+let checkedAnswers = 0
+
+const answerHandle = e => {
+  if (e.target.checked)
+    checkedAnswers ++
+  else
+    checkedAnswers--
+
+  console.log(checkedAnswers, answers)
+}
+
+answers.forEach(answer => {
+  addEventListener('change', answerHandle)
+})
